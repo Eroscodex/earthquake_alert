0c31e09eaf204d0ff997357f7e75c24e3ea77d68
Karl Nicko Alondra <karlnickoalondra02@gmail.com>
2026-07-03 14:55:26 +0800

    first commit

---
+++ src/api/phivolcs.js
@@ -0,0 +1,21 @@
+export default async function handler(req, res) {
+  try {
+    const response = await fetch(
+      'https://earthquake.phivolcs.dost.gov.ph/',
+      {
+        headers: {
+          'User-Agent': 'Mozilla/5.0',
+        },
+      }
+    )
+
+    const html = await response.text()
+
+    res.setHeader('Cache-Control', 'no-store')
+    res.status(200).send(html)
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    })
+  }
+}
